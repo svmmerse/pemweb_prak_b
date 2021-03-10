839a23f6d6b1f54be010c549c4d1bbb93850c822
Mohammad Syafiq <71855423+svmmerse@users.noreply.github.com>
2021-03-10 19:42:50 +0700

    yang baruu

--- Pertemuan 1/index.php
+++ Pertemuan 1/index.php
@@ -1,10 +1,45 @@
+<?php
+    $contoh_int = 1;
+    $contoh_str = "aku"
+
+    $array = ['pemro','website','kelas b','sem4'];
+    foreach ($array as $value) {
+        echo $value . "";
+    } 
+
+    for ($i =0; $i < count($array); $i++){
+        echo $array [$i]. "<br>";
+    }
+
+    $mhs = [
+        [
+        'nama' => 'syafiq',
+        'nim' => '192410101071',
+        'usia' => '19'
+        ],
+        ['nama' => 'mohammad',
+        'nim' => "1924101071",
+        'usia' => '20'
+        ],
+    ];
+
+    switch ($mahasiswa) {
+        case $mahasiswa['nama'] === 'firman':
+            echo $mahasiswa['nama'];
+            break;
+        default :
+            break;
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>"Hello World"</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 1</title>
 </head>
 <body>
-    <?php echo "Hello World!";?>
+
 </body>
 </html>
